71b1f2c0e5ad5702ba063f296a9af8d2a5081b5e
Justin Flesch <fleschju@flip3.engr.oregonstate.edu>
2021-03-06 17:19:08 -0800

    Fixing first test on fizzbuzz, still having issues understanding problem

---
+++ fizzbuzz.py
@@ -0,0 +1,34 @@
+# Cube program
+
+def avg_list(items):
+    value = 0
+    value = float(value)
+    if len(items) > 0:
+        for i in items:
+            try:
+                value = value + float(i)
+            except:
+                print("Error: Invalid value passed")
+                return
+        value = value / len(items)
+        return value
+    else:
+        print("Error: Zero values in list")
+        return
+
+class avg:
+    def __init__(self):
+        num1 = input("Enter a value: ")
+        if num1 == "end":
+            result = avg_list(items)
+            print("Average is: ", result)
+            quit()
+        else:
+            try:
+                items.append(num1)
+            except:
+                print("Error: Bad input")
+                break
+
+if __name__ == "__main__":
+    prompt = avg()
